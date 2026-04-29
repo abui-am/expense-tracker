d76e1491245820012742ad83017e80b49491f01e
adjiemuliadi <adjiem31@gmail.com>
2026-04-30 00:46:17 +0800

    Initial Commit

---
+++ costa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  costa
+//
+//  Created by Abuidillah Adjie Muliadi on 30/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ costa/costaApp.swift
@@ -0,0 +1,17 @@
+//
+//  costaApp.swift
+//  costa
+//
+//  Created by Abuidillah Adjie Muliadi on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct costaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
